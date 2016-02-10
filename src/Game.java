3aed71785017abc78f474f6da704d6fa1aa26bff
PiratePowWow <Thi3v3ry>
2016-02-10 11:59:49 -0500

    2.2 - Added Save and Load

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,10 @@
+import jodd.json.JsonParser;
+import jodd.json.JsonSerializer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -10,14 +17,24 @@ public class Game {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome, traveler.");
 
-        player.chooseName();
-        player.chooseWeapon();
-        player.chooseLocation();
+        try {
+            player = loadGame();
+            System.out.println("Loading saved game.");
+        }
+        catch (Exception e){
+            System.out.println("Starting new game.");
+        }
 
-        player.findItem("shield");
-        player.findItem("boots");
-        player.findItem("belt");
-        player.findItem("helm");
+        if (player.name == null) player.chooseName();
+        if (player.weapon == null) player.chooseWeapon();
+        if (player.location == null) player.chooseLocation();
+
+        if (player.items.isEmpty()) {
+            player.findItem("shield");
+            player.findItem("boots");
+            player.findItem("belt");
+            player.findItem("helm");
+        }
 
 
         Enemy ogre = new Enemy("Ogre", 10, 10);
@@ -48,6 +65,14 @@ public class Game {
                 case "/exit":
                     System.exit(0);
                     break;
+                case "/save":
+                    try {
+                        saveGame();
+                        System.out.println("Saved Game");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
                 default:
                     System.out.println("Command not found!");
                     break;
@@ -55,6 +80,26 @@ public class Game {
             line = scanner.nextLine();
         }
         return line;
+    }
+
+    public static void saveGame() throws IOException {
+        JsonSerializer serializer = new JsonSerializer();
+        String json = serializer.include("*").serialize(player);
+        File savegame = new File("savegame.json");
+        FileWriter fw = new FileWriter(savegame);
+        fw.write(json);
+        fw.close();
+    }
+
+    public static Player loadGame() throws FileNotFoundException {
+        File f = new File("savegame.json");
+        Scanner s = new Scanner(f);
+        s.useDelimiter("\\Z");
+        String contents = s.next();
+
+        JsonParser p = new JsonParser();
+        return p.parse(contents, Player.class);
+
     }
 
     //
